25658e1bfda7777789c586848e134c7b42d256d5
Lynn <lneir@yahoo.com>
2015-05-08 18:31:37 -0700

    archive results to targets dir

--- minuet/symphony/build-msi.cmd
+++ minuet/symphony/build-msi.cmd
@@ -1,8 +1,42 @@
-cd minuet\symphony\Installer
-set AIP=Minuet-x86
+@echo off
 
-set pfx=Wrapper.Windows.Product.RSA.pkcs12.pfx
-copy "%CEFDIR%\%pfx%" %pfx%
+set proc=%1
+
+rem AdvancedInstaller.com must be in PATH env
+
+if NOT DEFINED WRAPPER_VER (
+	echo "environment variable WRAPPER_VER not defined"
+	exit /b -1
+)
+
+echo "creating targets directory"
+rmdir /q /s targets
+mkdir targets
+set targetsDir="%CD%\targets\"
+
+rem tools dependencies, must be in your path
+set zip=7z.exe
+
+echo "create exec zip"
+cd minuet\symphony\bin
+set relArchive=minuet-x86-release-%WRAPPER_VER%.zip
+%zip% a -x!*.pdb -x!*app.publish -tZip %relArchive%
+xcopy %relArchive% %targetsDir%
+cd ..\..\..
+
+echo "creating msi %proc%"
+
+set pfxFile=Wrapper.Windows.Product.RSA.pkcs12.pfx
+if NOT EXIST "%CEFDIR%\%pfxFile%" (
+echo "can not find .pfx file" "%CEFDIR%\%pfxFile%"
+exit /b -1
+)
+
+copy /y "%CEFDIR%\%pfxFile%" "%installerDir%\%pfxFile%"
+
+cd minuet\symphony\Installer
+
+set AIP=Minuet-x86
 
 if EXIST %AIP%-cache (
 	echo "remove old msi cache file"
@@ -34,3 +68,5 @@ if NOT EXIST %AIP%-SetupFiles/%AIP%.msi (
 
 echo "copy result to target dir"
 xcopy "%AIP%-SetupFiles\%AIP%.msi" %targetsDir% /i
+set msiRelArchive=minuet-x86-release-%WRAPPER_VER%.msi
+move "%targetDir%\%AIP%.msi" "%targetDir%\%msiRelArchive%"
