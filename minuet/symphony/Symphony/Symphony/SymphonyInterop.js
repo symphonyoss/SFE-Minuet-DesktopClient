887088c6685088f43f1683616aca8259eaf31b2c
stamaw <william.stamatakis@gs.com>
2015-05-11 15:25:47 -0400

    Added new fews features
    New Features:
    
    - Publish System Interop status on topic system.events.symphony.com
    - Support incoming message (action:"Query_System_Status"} on topic system.symphony.com
    
    and clean up other code

--- minuet/symphony/Symphony/Symphony/SymphonyInterop.js
+++ minuet/symphony/Symphony/Symphony/SymphonyInterop.js
@@ -1,25 +1,27 @@
 var symphony = symphony || {};
 symphony.interop = {createChannel: function(){
     var mb = meow.messagebroker();
+    var sysstatus = "Ready";
+
     mb.subscribe("api.symphony.com", function (envelope) {
-  		var strEnvelope =  JSON.stringify(envelope);
-		console.log('symphonyInterop is working on:', strEnvelope);
-		console.log("envelope", envelope);
-
-		var msg;
-		if (envelope.message.hasOwnProperty('content')){
-			msg = JSON.parse(envelope.message.content);
-		} else {
-			msg = envelope.message;
-		}
-
-		console.log("msg", msg);
-        window.postMessage(msg, "*")
+        window.postMessage(envelope.message, "*");
     });
-  
+
+    mb.subscribe("system.symphony.com", function (envelope) {
+        if (envelope.replyAddress != null && envelope.message.action == "Query_System_Status") {
+            mb.send(envelope.replyAddress, {type: "System_Status", status: sysstatus});
+        }
+    });
+   
+    mb.onDisconnected.addListener(function () {
+        sysstatus = "offline";
+        mb.publish("system.events.symphony.com", { type: "System_Status", status: sysstatus });
+    });
+
+    mb.publish("system.events.symphony.com", { type: "System_Status", status: sysstatus});
+
     symphony.interop.outboundChannel = {
         post: function (message) {
-			console.log("Posting out : ", message);
             if (message != null && message.responseAddress != null)
             {
                 mb.send(message.responseAddress, message);
