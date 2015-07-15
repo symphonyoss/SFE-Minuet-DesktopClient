92369956f1cd584dec952617855a777845da623d
kaushikchaubal <kaushikchaubal@gmail.com>
2015-07-15 16:18:03 +0100

    DES-3542: Making the popup modal

--- minuet/symphony/Paragon.Runtime/CefHandlers/CefWebRequestHandler.cs
+++ minuet/symphony/Paragon.Runtime/CefHandlers/CefWebRequestHandler.cs
@@ -3,10 +3,33 @@
 using Xilium.CefGlue;
 using Paragon.Runtime.WinForms;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Interop;
 
 
 namespace Paragon.Runtime
 {
+    public class WindowWrapper : System.Windows.Forms.IWin32Window
+    {
+        public WindowWrapper(IntPtr handle)
+        {
+            _hwnd = handle;
+        }
+
+        public WindowWrapper(Window window)
+        {
+            _hwnd = new WindowInteropHelper(window).Handle;
+        }
+
+        public IntPtr Handle
+        {
+            get { return _hwnd; }
+        }
+
+        private IntPtr _hwnd;
+    }
+
     internal sealed class CefWebRequestHandler : CefRequestHandler
     {
         private static readonly ILogger Logger = ParagonLogManager.GetLogger();
@@ -52,8 +75,12 @@ namespace Paragon.Runtime
         }
         protected override bool GetAuthCredentials(CefBrowser browser, CefFrame frame, bool isProxy, string host, int port, string realm, string scheme, CefAuthCallback callback)
         {
+            string strFriendlyName = AppDomain.CurrentDomain.FriendlyName;
+            Process[] pro = Process.GetProcessesByName(strFriendlyName.Substring(0, strFriendlyName.LastIndexOf('.')));
+            System.Windows.Forms.IWin32Window handle = new WindowWrapper(pro[0].MainWindowHandle);
+
             LoginAuthenticationForm NewLogin = new LoginAuthenticationForm(host);
-            DialogResult Result = NewLogin.ShowDialog();
+            DialogResult Result = NewLogin.ShowDialog(handle);
             switch (Result)
             {
                 case DialogResult.OK:
--- minuet/symphony/Paragon.Runtime/WinForms/LoginAuthenticationForm.Designer.cs
+++ minuet/symphony/Paragon.Runtime/WinForms/LoginAuthenticationForm.Designer.cs
@@ -49,7 +49,7 @@
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
-            this.lblUserName.Location = new System.Drawing.Point(35, 28);
+            this.lblUserName.Location = new System.Drawing.Point(52, 31);
             this.lblUserName.Name = "lblUserName";
             this.lblUserName.Size = new System.Drawing.Size(60, 13);
             this.lblUserName.TabIndex = 1;
@@ -57,14 +57,14 @@
             // 
             // txtboxUserName
             // 
-            this.txtboxUserName.Location = new System.Drawing.Point(101, 25);
+            this.txtboxUserName.Location = new System.Drawing.Point(136, 28);
             this.txtboxUserName.Name = "txtboxUserName";
             this.txtboxUserName.Size = new System.Drawing.Size(178, 20);
             this.txtboxUserName.TabIndex = 2;
             // 
             // txtboxPassword
             // 
-            this.txtboxPassword.Location = new System.Drawing.Point(101, 51);
+            this.txtboxPassword.Location = new System.Drawing.Point(136, 58);
             this.txtboxPassword.Name = "txtboxPassword";
             this.txtboxPassword.PasswordChar = '*';
             this.txtboxPassword.Size = new System.Drawing.Size(178, 20);
@@ -73,7 +73,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(42, 54);
+            this.lblPassword.Location = new System.Drawing.Point(52, 61);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 3;
@@ -81,7 +81,7 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(69, 84);
+            this.btnLogin.Location = new System.Drawing.Point(66, 90);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 5;
@@ -92,7 +92,7 @@
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(164, 84);
+            this.btnCancel.Location = new System.Drawing.Point(220, 90);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 6;
@@ -106,7 +106,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(308, 125);
+            this.ClientSize = new System.Drawing.Size(347, 125);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtboxPassword);
@@ -118,9 +118,10 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginAuthenticationForm";
+            this.RightToLeftLayout = true;
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Authentication Required";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.LoginAuthenticationForm_Load);
