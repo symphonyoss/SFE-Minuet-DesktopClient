c2c804cd9fbe70a7c0d13087d041d435b33da403
unknown <daviga@dcnds0021864.firmwide.corp.gs.com>
2016-03-31 17:27:52 +0100

    fix #51

--- minuet/symphony/build.ps1
+++ minuet/symphony/build.ps1
@@ -37,28 +37,7 @@ Function Build([string]$config, [string]$platform)
 		write-host "failed building $config $platform"
 		exit -1 
 	}
-	msbuild Symphony.sln /t:clean /p:Configuration=$config /p:Platform=$platform
-	msbuild Symphony.sln /t:rebuild /p:Configuration=$config /p:Platform=$platform
-	$last=$?
-	if ($last -eq $False) 
-	{
-		write-host "failed building $config $platform"
-		exit -1 
-	}
 }
-
-# update manifest.json with build version
-$manifestFile = 'Symphony\Symphony\manifest.json'
-if ((Test-Path $manifestFile) -eq $False) {
-	echo 'abort: manifest.json file does not exist: ' $manifestFile
-	exit -1
-}
-$newVer=(get-childitem env:WRAPPER_VER).Value
-$newVersion='"version": "' + $newVer + '",'
-$content = (Get-Content $manifestFile)
-# replace line with "version":
-$newContent = $content -replace '^\s*"version"\s*:.*$', $newVersion
-Set-Content $manifestFile $newContent
 
 # build stuff
 Build "Release" "x86"
