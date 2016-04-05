0da63954b22e272517db3b994c92e7ea9868655e
Lynn <lynn@symphony.com>
2016-04-05 13:39:50 -0700

    DES-8191: activity detector plugin

---
+++ minuet/symphony/Symphony/Symphony/Plugins/ActivityPlugin.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using Paragon.Plugins;
+using System.Runtime.InteropServices;
+
+namespace Symphony.Plugins
+{
+    /* 
+     * This plugin looks for keyboard or mouse activity anywhere in the OS and raises
+     * event when activity occurs.  A timer running once per minutes checks for activity and
+     * only raises event if there has been activity within the last minute.
+     */
+    
+    [JavaScriptPlugin(Name = "symphony.activityDetector", IsBrowserSide = true)]
+    public class ActivityPlugin : IParagonPlugin
+    {
+        [JavaScriptPluginMember(Name = "onActivity")]
+        public event JavaScriptPluginCallback onActivity;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public POINT(System.Drawing.Point pt) : this(pt.X, pt.Y) { }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Int32 vKey);
+
+        int currX = -1;
+        int currY = -1;
+
+        System.Threading.Timer timer;
+
+        double checkIntervalInSecs = 5.0;
+
+        public void Initialize(IApplication application)
+        {
+            timer = new System.Threading.Timer(onTimer, null, TimeSpan.Zero, 
+                TimeSpan.FromSeconds(checkIntervalInSecs));
+        }
+
+        // check for mouse position changes every 60 seconds
+        void onTimer(object state)
+        {
+            if (anyMouseMovement() || anyKeyPressed())
+            {
+                var evnt = onActivity;
+                if (evnt != null)
+                    evnt();
+            }            
+        }
+
+        bool anyMouseMovement()
+        {
+            POINT p;
+            if (GetCursorPos(out p))
+            {
+                if (p.X != currX || p.Y != currY)
+                {
+                    bool isCurrValueInit = (currX == -1 && currY == -1);
+
+                    currX = p.X;
+                    currY = p.Y;
+
+                    if (!isCurrValueInit)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        enum Keys : int
+        {
+            ///<summary>
+            ///BACKSPACE key
+            ///</summary>
+            BACK = 0x08,
+            ///<summary>
+            ///TAB key
+            ///</summary>
+            TAB = 0x09,
+            ///<summary>
+            ///ENTER key
+            ///</summary>
+            RETURN = 0x0D,
+            ///<summary>
+            ///SHIFT key
+            ///</summary>
+            SHIFT = 0x10,
+            ///<summary>
+            ///CTRL key
+            ///</summary>
+            CONTROL = 0x11,
+            ///<summary>
+            ///CAPS LOCK key
+            ///</summary>
+            CAPITAL = 0x14,
+            ///<summary>
+            ///Input Method Editor (IME) Kana mode
+            ///</summary>
+            KANA = 0x15,
+            ///<summary>
+            ///IME Hangul mode
+            ///</summary>
+            HANGUL = 0x15,
+            ///<summary>
+            ///IME Junja mode
+            ///</summary>
+            JUNJA = 0x17,
+            ///<summary>
+            ///IME final mode
+            ///</summary>
+            FINAL = 0x18,
+            ///<summary>
+            ///IME Hanja mode
+            ///</summary>
+            HANJA = 0x19,
+            ///<summary>
+            ///IME Kanji mode
+            ///</summary>
+            KANJI = 0x19,
+            ///<summary>
+            ///ESC key
+            ///</summary>
+            ESCAPE = 0x1B,
+            ///<summary>
+            ///SPACEBAR
+            ///</summary>
+            SPACE = 0x20,
+            ///<summary>
+            ///LEFT ARROW key
+            ///</summary>
+            LEFT = 0x25,
+            ///<summary>
+            ///UP ARROW key
+            ///</summary>
+            UP = 0x26,
+            ///<summary>
+            ///RIGHT ARROW key
+            ///</summary>
+            RIGHT = 0x27,
+            ///<summary>
+            ///DOWN ARROW key
+            ///</summary>
+            DOWN = 0x28,
+            ///<summary>
+            ///DEL key
+            ///</summary>
+            DELETE = 0x2E,
+            ///<summary>
+            ///0 key
+            ///</summary>
+            KEY_0 = 0x30,
+            ///<summary>
+            ///1 key
+            ///</summary>
+            KEY_1 = 0x31,
+            ///<summary>
+            ///2 key
+            ///</summary>
+            KEY_2 = 0x32,
+            ///<summary>
+            ///3 key
+            ///</summary>
+            KEY_3 = 0x33,
+            ///<summary>
+            ///4 key
+            ///</summary>
+            KEY_4 = 0x34,
+            ///<summary>
+            ///5 key
+            ///</summary>
+            KEY_5 = 0x35,
+            ///<summary>
+            ///6 key
+            ///</summary>
+            KEY_6 = 0x36,
+            ///<summary>
+            ///7 key
+            ///</summary>
+            KEY_7 = 0x37,
+            ///<summary>
+            ///8 key
+            ///</summary>
+            KEY_8 = 0x38,
+            ///<summary>
+            ///9 key
+            ///</summary>
+            KEY_9 = 0x39,
+            ///<summary>
+            ///A key
+            ///</summary>
+            KEY_A = 0x41,
+            ///<summary>
+            ///B key
+            ///</summary>
+            KEY_B = 0x42,
+            ///<summary>
+            ///C key
+            ///</summary>
+            KEY_C = 0x43,
+            ///<summary>
+            ///D key
+            ///</summary>
+            KEY_D = 0x44,
+            ///<summary>
+            ///E key
+            ///</summary>
+            KEY_E = 0x45,
+            ///<summary>
+            ///F key
+            ///</summary>
+            KEY_F = 0x46,
+            ///<summary>
+            ///G key
+            ///</summary>
+            KEY_G = 0x47,
+            ///<summary>
+            ///H key
+            ///</summary>
+            KEY_H = 0x48,
+            ///<summary>
+            ///I key
+            ///</summary>
+            KEY_I = 0x49,
+            ///<summary>
+            ///J key
+            ///</summary>
+            KEY_J = 0x4A,
+            ///<summary>
+            ///K key
+            ///</summary>
+            KEY_K = 0x4B,
+            ///<summary>
+            ///L key
+            ///</summary>
+            KEY_L = 0x4C,
+            ///<summary>
+            ///M key
+            ///</summary>
+            KEY_M = 0x4D,
+            ///<summary>
+            ///N key
+            ///</summary>
+            KEY_N = 0x4E,
+            ///<summary>
+            ///O key
+            ///</summary>
+            KEY_O = 0x4F,
+            ///<summary>
+            ///P key
+            ///</summary>
+            KEY_P = 0x50,
+            ///<summary>
+            ///Q key
+            ///</summary>
+            KEY_Q = 0x51,
+            ///<summary>
+            ///R key
+            ///</summary>
+            KEY_R = 0x52,
+            ///<summary>
+            ///S key
+            ///</summary>
+            KEY_S = 0x53,
+            ///<summary>
+            ///T key
+            ///</summary>
+            KEY_T = 0x54,
+            ///<summary>
+            ///U key
+            ///</summary>
+            KEY_U = 0x55,
+            ///<summary>
+            ///V key
+            ///</summary>
+            KEY_V = 0x56,
+            ///<summary>
+            ///W key
+            ///</summary>
+            KEY_W = 0x57,
+            ///<summary>
+            ///X key
+            ///</summary>
+            KEY_X = 0x58,
+            ///<summary>
+            ///Y key
+            ///</summary>
+            KEY_Y = 0x59,
+            ///<summary>
+            ///Z key
+            ///</summary>
+            KEY_Z = 0x5A,
+            ///<summary>
+            ///Numeric keypad 0 key
+            ///</summary>
+            NUMPAD0 = 0x60,
+            ///<summary>
+            ///Numeric keypad 1 key
+            ///</summary>
+            NUMPAD1 = 0x61,
+            ///<summary>
+            ///Numeric keypad 2 key
+            ///</summary>
+            NUMPAD2 = 0x62,
+            ///<summary>
+            ///Numeric keypad 3 key
+            ///</summary>
+            NUMPAD3 = 0x63,
+            ///<summary>
+            ///Numeric keypad 4 key
+            ///</summary>
+            NUMPAD4 = 0x64,
+            ///<summary>
+            ///Numeric keypad 5 key
+            ///</summary>
+            NUMPAD5 = 0x65,
+            ///<summary>
+            ///Numeric keypad 6 key
+            ///</summary>
+            NUMPAD6 = 0x66,
+            ///<summary>
+            ///Numeric keypad 7 key
+            ///</summary>
+            NUMPAD7 = 0x67,
+            ///<summary>
+            ///Numeric keypad 8 key
+            ///</summary>
+            NUMPAD8 = 0x68,
+            ///<summary>
+            ///Numeric keypad 9 key
+            ///</summary>
+            NUMPAD9 = 0x69
+        }
+
+        bool anyKeyPressed()
+        {
+            bool wasKeyPressed = false;
+            // need to loop through all key otherwise if multiple keys are hit then event
+            // would be raised on next period.
+            foreach (System.Int32 i in Enum.GetValues(typeof(Keys)))
+            {
+                int x = GetAsyncKeyState(i);
+                // detect key down or up.
+                if ((x == 1) || (x == Int16.MinValue))
+                    wasKeyPressed = true;
+            }
+
+            return wasKeyPressed;
+        }
+
+        public void Shutdown()
+        {
+            timer.Dispose();
+        }
+    }
+}
