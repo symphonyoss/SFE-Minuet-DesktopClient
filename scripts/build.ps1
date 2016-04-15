adfbfe24478ec932239a28475311fc367ea8da2e
Gareth Davies <gareth.davies@gs.com>
2016-04-15 16:09:18 +0100

    updated build script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,12 +1,3 @@
-echo "go to minuet/symphony folder"
-cd minuet
-cd symphony
-
-echo "make output dirs"
-md bin
-md bin\apps
-md bin\paragon
-
 echo "Set Visual Studio env vars"
 $path1='c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
 $path2='c:\Program Files\Microsoft Visual Studio 12.0\VC'
@@ -29,16 +20,8 @@ write-host "`nVisual Studio 2013 Command Prompt variables set." -ForegroundColor
 Function Build([string]$config, [string]$platform)
 {
 	echo "Starting $config $platform build"
-	msbuild Paragon.sln /t:clean /p:Configuration=$config /p:Platform=$platform
-	msbuild Paragon.sln /t:rebuild /p:Configuration=$config /p:Platform=$platform
-	$last=$?
-	if ($last -eq $False) 
-	{
-		write-host "failed building $config $platform"
-		exit -1 
-	}
-	msbuild Symphony.sln /t:clean /p:Configuration=$config /p:Platform=$platform
-	msbuild Symphony.sln /t:rebuild /p:Configuration=$config /p:Platform=$platform
+	msbuild Minuet.sln /t:clean /p:Configuration=$config /p:Platform=$platform
+	msbuild Minuet.sln /t:rebuild /p:Configuration=$config /p:Platform=$platform
 	$last=$?
 	if ($last -eq $False) 
 	{
@@ -46,19 +29,6 @@ Function Build([string]$config, [string]$platform)
 		exit -1 
 	}
 }
-
-# update manifest.json with build version
-$manifestFile = 'Symphony\Symphony\manifest.json'
-if ((Test-Path $manifestFile) -eq $False) {
-	echo 'abort: manifest.json file does not exist: ' $manifestFile
-	exit -1
-}
-$newVer=(get-childitem env:WRAPPER_VER).Value
-$newVersion='"version": "' + $newVer + '",'
-$content = (Get-Content $manifestFile)
-# replace line with "version":
-$newContent = $content -replace '^\s*"version"\s*:.*$', $newVersion
-Set-Content $manifestFile $newContent
 
 # build stuff
 Build "Release" "x86"
