23dc739d3f9bd5fa8c752d50fb9ada5027cc0b13
Gareth Davies <gareth.davies@gs.com>
2016-04-15 19:56:43 +0100

    improving sample build script

--- scripts/build-sample.ps1
+++ scripts/build-sample.ps1
@@ -1,9 +1,13 @@
-md bin\apps
-
-if (!(Test-Path bin\paragon\Paragon.AppPackager)) { 
-    echo 'Minuet AppPackager (Paragon.AppPackager.exe) not found.  Ensure Minuet has been build: powershell ./scripts/build.ps1'
+if (!(Test-Path bin\paragon\Paragon.AppPackager.exe)) { 
+    write-host 'Minuet AppPackager (Paragon.AppPackager.exe) not found.  Ensure Minuet has been built `powershell ./scripts/build.ps1`' -ForegroundColor Red
     exit -1
-}; 
+};
+
+if (Test-Path bin\apps) {
+    rd bin\apps -recurse 
+}
+
+md bin\apps 
 
 $build_command = 'bin\paragon\Paragon.AppPackager --i:sample-app --o:bin\apps\sample.pgx'
 iex $build_command
