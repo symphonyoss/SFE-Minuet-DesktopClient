714dbff5c12464feb57fbeb7711a2be19cde977a
Gareth Davies <gareth.davies@gs.com>
2016-04-18 14:58:25 +0100

    Remove AuthServerWhitelist and AuthDelegateWhitelist as no longer supported in CEF 3.2526.1359.gced449a - Chromium 47.0.2526.73

--- minuet/Paragon.Runtime/ParagonRuntime.cs
+++ minuet/Paragon.Runtime/ParagonRuntime.cs
@@ -62,8 +62,6 @@ namespace Paragon.Runtime
         public static void Initialize(string cachePath = null,
                                       string paragonPath = null,
                                       string browserLanguage = null,
-                                      string authserverlist = null,
-                                      string authdelgatelist = null,
                                       bool disableSpellChecking = false,
                                       bool ignoreCertificateErrors = false,
                                       bool persistSessionCookies = false)
@@ -126,8 +124,6 @@ namespace Paragon.Runtime
                         Locale = browserLanguage,
                         CachePath = cachePath,
                         PersistSessionCookies = persistSessionCookies,
-                        AuthServerWhitelist = authserverlist,
-                        AuthDelegateWhitelist = authdelgatelist,
                         ProductVersion = string.Format("Paragon/{0} Chrome/{1}", Assembly.GetExecutingAssembly().GetName().Version, CefRuntime.ChromeVersion)
                         
                     };
