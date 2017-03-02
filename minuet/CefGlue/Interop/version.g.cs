03dc837816fa18ca205b96f35464fff610697a2c
Rodrigo de Passos Barros <rbarros@daitangroup.com>
2017-03-02 14:23:00 -0300

    move to chromium v56

--- minuet/CefGlue/Interop/version.g.cs
+++ minuet/CefGlue/Interop/version.g.cs
@@ -9,20 +9,20 @@ namespace Xilium.CefGlue.Interop
     
     internal static unsafe partial class libcef
     {
-        public const string CEF_VERSION = "3.2883.1539.gd7f087e";
+        public const string CEF_VERSION = "3.2924.1571.gcdcdfa9";
         public const int CEF_VERSION_MAJOR = 3;
-        public const int CEF_COMMIT_NUMBER = 1539;
-        public const string CEF_COMMIT_HASH = "d7f087e76634af8ceffdef28c794b7310bb507e5";
+        public const int CEF_COMMIT_NUMBER = 1571;
+        public const string CEF_COMMIT_HASH = "cdcdfa99913a01f3617a0d25a2e2f2ed145987bd";
         
-        public const int CHROME_VERSION_MAJOR = 55;
+        public const int CHROME_VERSION_MAJOR = 56;
         public const int CHROME_VERSION_MINOR = 0;
-        public const int CHROME_VERSION_BUILD = 2883;
-        public const int CHROME_VERSION_PATCH = 59;
+        public const int CHROME_VERSION_BUILD = 2924;
+        public const int CHROME_VERSION_PATCH = 76;
         
-        public const string CEF_API_HASH_UNIVERSAL = "95aeb3fed6b2c813abe66fda168b5176cc520f6e";
+        public const string CEF_API_HASH_UNIVERSAL = "66de193ba22e1d92a99bb29d60f3107709aeefda";
         
-        public const string CEF_API_HASH_PLATFORM_WIN = "00823905486d7b7222da5654fe35d2d15f65543a";
-        public const string CEF_API_HASH_PLATFORM_MACOSX = "6361eb5f8e0440c4e103f1c95373eef83c16f242";
-        public const string CEF_API_HASH_PLATFORM_LINUX = "37b7bf683bd6d8d858acbaba00f30796ca530b2a";
+        public const string CEF_API_HASH_PLATFORM_WIN = "8055740cd08db66cefe838a826dc90806fadfb33";
+        public const string CEF_API_HASH_PLATFORM_MACOSX = "12d8ab423df369b68d37c3667123a1812bc0d345";
+        public const string CEF_API_HASH_PLATFORM_LINUX = "86ab23c0d7dafbdff7f66764cf8dac5ec1712af4";
     }
 }
